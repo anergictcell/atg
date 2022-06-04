1d8213cea1aca0e0382fedaa04fcc03706458d60
Jonas Marcello <jonas.marcello@esbme.com>
2022-06-04 14:45:33 +0200

    Remove all lib sections and only keep CLI functionality

--- src/bed/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Convert to bed format
-//!
-//! bed does not provide a Reader instance, can only be used as output
-mod record;
-mod writer;
-
-pub use crate::bed::record::BedLine;
-pub use crate::bed::writer::Writer;
